1a1274b05acc16bfbffe606c2d0339acd8f1504c
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-08 17:21:26 +0100

    feat: integrating react app

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
 
 const localGraphQL = "http://localhost:4000/graphql";
 
 const client = new ApolloClient({
   uri: localGraphQL
 });
+
+
+// import { Query } from "react-apollo";
+
+const getRates = gql`
+{
+  products(type: "DVD") {
+    name
+    price
+  }
+}`;
+// const Data = () => (
+//   <Query query={getRates} >
+//   {({ loading, error, data }) => {
+//   if (loading) return <p>Loading…</p>;
+//   if (error) return <p>Error :(</p>;
+//   return data.products.map(({ name, price }) => (
+//     <div key={name}>
+//       <p>{`${name}: ${price}`}</p>
+//     </div>
+//    ));
+//   }}
+// </Query>
+// );
+// export default Data;
 
 function App() {
   return (
@@ -37,3 +63,6 @@ function App() {
 }
 
 export default App;
+
+
+
