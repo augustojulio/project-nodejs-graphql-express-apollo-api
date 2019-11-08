b60e7b09f56d6b984b50c556f468258f05300793
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-08 16:33:12 +0100

    feat: integrating react app

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Local from “./components/Local”;
+
+const localGraphQL = "http://localhost:4000/graphql";
+
+const client = new ApolloClient({
+  uri: localGraphQL
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
