75907da4a5a0d5472ee5097d0bbe31625a4fd518
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-16 18:57:09 +0100

    refactor: moving api code to right directory

---
+++ api/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express'); 
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const app = express();
+
+const PORT = 4000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.listen(PORT, () =>
+    console.log(`Server ready at http://localhost:4000/graphql`))
