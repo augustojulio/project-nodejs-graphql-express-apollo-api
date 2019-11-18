c4390b0ab783d3268b34081bd7e75da3eaa2d792
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-18 13:18:24 +0100

    test: added mocha chai tests

--- server-tests/server-tests.js
+++ server-tests/server-tests.js
@@ -1,45 +1,70 @@
 const chai = require('chai');
 const gql = require('graphql-tag');
 
+const chaiGraphQL = require('chai-graphql');
+chai.use(chaiGraphQL)
+const assert = require('chai').assert;
+
 const expect = chai.expect;
 const url = `http://localhost:4000/`;
 const request = require('supertest')(url);
 
-const getAllIcos = gql`
-{
-  allIcos {
-        address
-        currency
-        value
-        txid
-    }
-}`;
+// const getAllIcos = gql`
+// {
+//   allIcos {
+//         address
+//         currency
+//         value
+//         txid
+//     }
+// }`;
 
-describe('GraphQL', () => {
-    it('Returns ico with value = 146859', (done) => {
-        request.post('/graphql')
-        .send({ query: '{ allIcos(value: 146859) { value address currency txid } }'})
-        .expect(200)
-        .end((err,res) => {
-            // res will contain array with one user
-            if (err) return done(err);
-            res.body.allIcos.should.have.property('value')
-            res.body.allIcos.should.have.property('address')
-            res.body.allIcos.should.have.property('currency')
-            res.body.allIcos.should.have.property('txid')
-            done();
-        })
-    })
+var goodResponse = {
+  data: {
+    foo: 'bar'
+  }
+}
 
-    it('Returns all icos', (done) => {
-        request.post('/graphql')
-        .send({ query: '{ allIcos { value address currency txid } }' })
-        .expect(200)
-        .end((err, res) => {
-            // res will contain array of all users
-            if (err) return done(err);
-            // assume there are a 100 users in the database
-            res.body.allIcos.should.have.lengthOf(100);
-        })
-    })
-});
+// Passes
+assert.graphQL(goodResponse, { foo: 'bar' })
+assert.graphQLSubset(goodResponse, { foo: 'bar' })
+assert.graphQL(goodResponse)
+assert.graphQLSubset(goodResponse)
+assert.graphQLSubset(goodResponse, { })
+assert.notGraphQLError(goodResponse)
+expect(goodResponse).to.be.graphQL({ foo: 'bar' })
+
+// Fails
+// assert.graphQL(goodResponse, { foo: 'FAIL' })
+// assert.graphQL(goodResponse, { })
+// assert.graphQLError(goodResponse)
+// expect(goodResponse).to.be.graphQLError()
+
+// describe('GraphQL', () => {
+//     it('Returns ico with value = 146859', (done) => {
+//         // request.post('/graphql')
+//         // .send({ query: '{ allIcos(value: 146859) { value address currency txid } }'})
+//         .expect(200)
+//         .end((err,res) => {
+//             // res will contain array with one user
+//             if (err) return done(err);
+//             res.body.allIcos.should.have.property('value')
+//             res.body.allIcos.should.have.property('address')
+//             res.body.allIcos.should.have.property('currency')
+//             res.body.allIcos.should.have.property('txid')
+//             done();
+//         })
+//     })
+
+//     it('Returns all icos', (done) => {
+//         request.post('/graphql')
+//         // .send({ query: '{ allIcos { value address currency txid } }' })
+//         .expect(200)
+//         .end((err, res) => {
+//             // res will contain array of all users
+//             if (err) return done(err);
+//             // assume there are a 100 users in the database
+//             res.body.allIcos.should.have.lengthOf(100);
+//         })
+//     })
+// });
